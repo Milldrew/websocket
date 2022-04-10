1622a693f50491de98820210a724ff2264474841
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-04-09 19:11:38 -0600

    📅  | Sat Apr  9 19:11:38 MDT 2022

--- 02-gateways/src/events/events.gateway.ts
+++ 02-gateways/src/events/events.gateway.ts
@@ -4,27 +4,31 @@ import {
   WebSocketGateway,
   WebSocketServer,
   WsResponse,
-} from '@nestjs/websockets';
-import { from, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { Server } from 'socket.io';
+} from "@nestjs/websockets";
+import { from, Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { Server } from "socket.io";
 
 @WebSocketGateway({
   cors: {
-    origin: '*',
+    origin: "*",
   },
 })
 export class EventsGateway {
   @WebSocketServer()
   server: Server;
 
-  @SubscribeMessage('events')
+  @SubscribeMessage("events")
   findAll(@MessageBody() data: any): Observable<WsResponse<number>> {
-    return from([1, 2, 3]).pipe(map(item => ({ event: 'events', data: item })));
+    return from([1, 2, 3]).pipe(
+      map((item) => ({ event: "events", data: item }))
+    );
   }
 
-  @SubscribeMessage('identity')
+  @SubscribeMessage("identity")
   async identity(@MessageBody() data: number): Promise<number> {
+    console.log("HELLO");
+    console.log(data);
     return data;
   }
 }
